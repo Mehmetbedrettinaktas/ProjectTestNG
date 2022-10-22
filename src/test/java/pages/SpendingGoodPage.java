320aa4d30c1513ba93f347c0c6c6dd4bf3fe9751
Mehmet Akin <maebtab.858715@gmail.com>
2022-10-22 15:35:13 +0200

    M3

--- src/test/java/pages/SpendingGoodPage.java
+++ src/test/java/pages/SpendingGoodPage.java
@@ -14,47 +14,201 @@ public class SpendingGoodPage {
     }
 
 
-    @FindBy(xpath = "//*[text()='Sign In ']")
-    public WebElement signIn;
 
-    @FindBy(xpath = "//*[@id='username']")
-    public WebElement userName;
 
-    @FindBy(xpath = "//*[@id='password']")
-    public WebElement password;
 
-    @FindBy(xpath = "//li[@id='menu-item-1074']")
-    public WebElement myAccount;
 
-    @FindBy(xpath = "//*[text()='Store Manager']")
-    public WebElement storeManager;
 
-    @FindBy(xpath = "//*[@class='wcfm_menu_items wcfm_menu_wcfm-products']")
-    public WebElement products;
 
-    @FindBy(xpath = "//*[@id='add_new_product_dashboard']")
-    public WebElement addNew;
 
-    @FindBy(xpath = "//*[@id='manage_stock']")
-    public WebElement manageStock;
 
-    @FindBy(xpath = "//*[@id='stock_qty']")
-    public WebElement stockQty;
 
-    @FindBy(xpath = "//*[@id='backorders']")
-    public WebElement backOrders;
 
-    @FindBy(xpath = "//*[text()='Shipping']")
-    public WebElement shipping;
 
-    @FindBy(xpath = "//*[@id='weight']")
-    public WebElement weight;
 
-    @FindBy(xpath = "//*[@id='_wcfmmp_processing_time']")
-    public WebElement processingTime;
 
-    @FindBy(xpath = "//*[@class='shipping_class wcfm_title']")
-    public WebElement shippingClass;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @FindBy(xpath = "//*[text()='Gateaway']")
@@ -63,9 +217,31 @@ public class SpendingGoodPage {
     @FindBy(xpath = "//*[@id='wcfm_products_simple_submit_button']")
     public WebElement submit;
 
-
     @FindBy(xpath = "(//*[contains(text(),'Product Successfully Published.')])[1]")
     public WebElement yazi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
