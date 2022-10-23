d1b2a072b7662565a540d6937c3239649e7103d7
Mehmet Bedrettin Aktas <104640212+Mehmetbedrettinaktas@users.noreply.github.com>
2022-10-23 20:52:22 +0200

    Update SpendingGoodPage.java

--- src/test/java/pages/SpendingGoodPage.java
+++ src/test/java/pages/SpendingGoodPage.java
@@ -79,7 +79,7 @@ public class SpendingGoodPage {
         @FindBy(xpath ="//*[text()='Registration Successfully Completed.']")
         public static WebElement gecersizPasswordHataliMesaji;
 
-        /////////////////////////////////////////////////////////////
+      
         //verification code div.email method classi
         @FindBy(css = "div.email")
         public static WebElement geciciEmail;
